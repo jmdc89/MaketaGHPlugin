b3a69b0f4d562081261fdb0a1359b67c9c8075c8
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2024-02-01 19:14:36 +0100

    Initial VS project

---
+++ src/maketa_ghpluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace maketa_ghplugin
+{
+    public class maketa_ghpluginInfo : GH_AssemblyInfo
+    {
+        public override string Name => "maketa_ghplugin";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("e64a246c-13b7-49ff-a0b3-858cc914205b");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
